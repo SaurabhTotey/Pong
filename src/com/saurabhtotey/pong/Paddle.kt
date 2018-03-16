90da4692378c5248ff6761636d7e4fdb44719e06
Saurabh Totey <SaurabhTotey@gmail.com>
2018-03-16 15:39:03 -0600

    Made basic Paddle class

--- src/com/saurabhtotey/pong/Paddle.kt
+++ src/com/saurabhtotey/pong/Paddle.kt
@@ -5,4 +5,24 @@ package com.saurabhtotey.pong
  * Are the only objects the players can directly control
  */
 class Paddle(val gameWidth: Int, val gameHeight: Int, override val count: Int) : GameObject() {
+
+    override val width = this.gameWidth.toFloat() / 50
+    override val height = this.gameHeight.toFloat() / 5
+    override var x = if (this.count == 0) 0.toFloat() else this.gameWidth - this.width
+    override var y = (this.gameHeight - this.height) / 2
+    override var xVelocity = 0.toFloat()
+    override var yVelocity = 0.toFloat()
+
+    /**
+     * Every tick, the game paddle stop moving
+     */
+    override fun tickAction() {
+        this.speed = 0.toFloat()
+    }
+
+    /**
+     * The paddle itself doesn't do anything on a collide
+     */
+    override fun onCollide(other: GameObject) {}
+
 }
